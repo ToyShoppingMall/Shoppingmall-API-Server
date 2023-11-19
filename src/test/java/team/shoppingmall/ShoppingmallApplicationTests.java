2d7e67ebf2c750b5e806fd1c78e82fb6cedf227a
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-11-19 20:57:31 +0900

    project init

---
+++ src/test/java/team/shoppingmall/ShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package team.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
